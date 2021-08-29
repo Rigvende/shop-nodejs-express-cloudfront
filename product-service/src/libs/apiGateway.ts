1213947d75df21cd08ae57fdc54f58cb790ab249
Marianna Patrusova <sumeragi.tanako@gmail.com>
2021-08-29 21:30:58 +0300

    adding headers for CORS

--- product-service/src/libs/apiGateway.ts
+++ product-service/src/libs/apiGateway.ts
@@ -4,9 +4,16 @@ import type { FromSchema } from "json-schema-to-ts";
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
+const HEADERS = {
+  "Access-Control-Allow-Headers": "*",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*"
+};
+
 export const formatJSONResponse = (response: Record<string, unknown>) => {
   return {
     statusCode: 200,
+    headers: HEADERS,
     body: JSON.stringify(response)
   }
 }
